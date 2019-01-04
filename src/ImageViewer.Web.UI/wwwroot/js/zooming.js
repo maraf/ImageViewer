dbe2f1cf102d4530dfad61fc5a1bd9902b6712ce
Marek Fišera <mara@neptuo.com>
2019-01-04 19:58:13 +0100

    #6 - Centering the image.

--- src/ImageViewer.Web.UI/wwwroot/js/zooming.js
+++ src/ImageViewer.Web.UI/wwwroot/js/zooming.js
@@ -3,10 +3,27 @@
 
     this._lastScale = 0;
     this._image.bind('touchy-pinch', this._OnPinch.bind(this));
+
+    var onImageChanged = this.OnImageChanged.bind(this);
+    image.addEventListener("load", onImageChanged, false);
+    window.addEventListener("resize", onImageChanged, false);
 };
 
 Zooming.prototype._OnPinch = function (e, $target, data) {
     this._lastScale = data.previousScale + data.scale;
 
     $target.css({ 'webkitTransform': 'scale(' + this._lastScale + ',' + this._lastScale + ')' });
+};
+
+Zooming.prototype.OnImageChanged = function () {
+    var imageWidth = this._image[0].width;
+    var imageHeight = this._image[0].height;
+
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
+
+    var offsetX = (screenWidth - imageWidth) / 2;
+    var offsetY = (screenHeight - imageHeight) / 2;
+
+    this._image.css({ left: offsetX, top: offsetY });
 };
